77827efc6c0d4aea30e6cb0744f227b68ad7f81e
SabreoftheNorth <deozerro29@gmail.com>
2026-04-27 20:35:01 +0700

    SplashScreen.java

---
+++ app/src/main/java/com/example/sampahku/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.sampahku;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    //durasi splash screen = 3 detik (3000 milidetik)
+    private static final int SPLASH_SCREEN_DURATION = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //untuk pindah ke MainActivity habis splash screen selesai
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_DURATION);
+    }
+}
